1c52bf01e2f5b94e98dbf5137e3fd805dfc41c4d
Omar Wael <omar_bazaraa@hotmail.com>
2017-02-24 12:08:46 +0200

    Merged IAR work but didn't check yet if they are working due to issues in OpenCV library.

--- Flow Solver/Source.cpp
+++ Flow Solver/Source.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <ctime>
-#include "Solver.h"
+#include "FlowSolver.h"
 using namespace std;
 
-int main(int argc, char* argv[]) {
+/**
+ * Used to boost reading/writing from/to the console
+ */
+inline void boostIO() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+/**
+ * Main function
+ */
+int main() {
+	boostIO();
 	int startTime = clock();
 
-	if (!init("mazes/maze10x10.txt")) {
-		return 0;
+	try {
+		FlowSolver solver("mazes/game.png");
+		solver.printMaze();
+		solver.solve();
+		solver.printSolution();
 	}
-
-	printMaze();
-	getPathes();
-	printSolution();
-
+	catch (const exception& ex) {
+		cout << "ERROR::" << ex.what() << endl;
+	}
+	
 	int stopTime = clock();
 	cout << "time: " << (stopTime - startTime) / double(CLOCKS_PER_SEC) * 1000.0 << "ms" << endl;
-
 	return 0;
 }
